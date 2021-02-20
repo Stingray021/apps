0f544d5b093d72be11361bb281969a5ada55651d
unknown <sasha.polovnikov.001@mail.ru>
2021-02-20 18:52:05 +0300

    first commit

--- Sample-Test/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "../ConsoleApplication1/ConsoleApplication1.cpp"
-#include "gtest/gtest.h"
-
-TEST(TestCaseName, TestName) {
-  char* str = findWords("aaa", 'a');
-  ASSERT_STREQ("aaa", str);
-}
